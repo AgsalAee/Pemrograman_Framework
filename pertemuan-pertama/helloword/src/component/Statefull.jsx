7221cf2d42744555e30c50bda7ae145b8dcc7d88
AgsalAee <agsal.agsal9@gmail.com>
2021-02-23 17:45:51 +0700

    Stateless dan Statefull
    week 3

---
+++ pertemuan-pertama/helloword/src/component/Statefull.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+class Statefull extends React.Component{
+    render(){
+      return<p>StateFullComponent</p>
+    }
+  }
+
+  export default Statefull;
--- pertemuan-pertama/helloword/src/index.js
+++ pertemuan-pertama/helloword/src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from "register-service-worker";
+import Login from './component/Login';
+// import { render } from '@testing-library/react';
+// import Statefull from './component/Statefull';
+// import Stateless from './component/Stateless';
+// import App from './App';
+// import List from './component/List';
+//import Test from './component/Test';
+//import HelloComponent from './component/HelloComponent';
+//import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Login/>
+    {/* <Statefull/>
+    <Stateless/> */}
+    {/* <App/> */}
+    {/* <Test/> */}
+    {/* <HelloComponent/> */}
+    </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+serviceWorker.unregister();
