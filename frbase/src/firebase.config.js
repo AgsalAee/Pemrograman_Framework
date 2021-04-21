73edca2dbc56afb7bbfe1ae9a80b4364dc0a5c37
AgsalAee <agsal.agsal9@gmail.com>
2021-04-21 10:40:23 +0700

    Firebase

---
+++ frbase/src/firebase.config.js
@@ -0,0 +1,8 @@
+export default{
+  apiKey: "AIzaSyBuQFOVk-6nTm5ZKCSKLxLWWma2Fis7Sso",
+authDomain: "fir-e4236.firebaseapp.com",
+projectId: "fir-e4236",
+storageBucket: "fir-e4236.appspot.com",
+messagingSenderId: "713024958270",
+appId: "1:713024958270:web:1a1a5c0c4a1031b52b866f",
+measurementId: "G-7JYPXKMPVQ"};
